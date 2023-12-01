420775ee93d5022a6673db760528fa5b7608a00f
Premagrahan <44720172+Premagrahan@users.noreply.github.com>
2023-12-01 17:39:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("🥣 My Parents New Healty Dinner")
-streamlit.header(🥗 "Breakfast menu")
+streamlit.header("🥗 Breakfast menu")
 streamlit.text("🐔 Omega 3 and blakberry oamlet")
 streamlit.text("🥑 kale,Spnish and Rocket Smoothe")
 streamlit.text("🍞Hard-boiled Free-Range Egg")
