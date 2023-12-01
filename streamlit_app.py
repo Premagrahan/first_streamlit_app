be1776219b327f5742a0e7ee13eee0d4e413f164
Premagrahan <44720172+Premagrahan@users.noreply.github.com>
2023-12-01 17:43:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 streamlit.title("🥣 My Parents New Healty Dinner")
 streamlit.header("🥗 Breakfast menu")
 streamlit.text("🐔 Omega 3 and blakberry oamlet")
 streamlit.text("🥑 kale,Spnish and Rocket Smoothe")
 streamlit.text("🍞Hard-boiled Free-Range Egg")
+
+streamlit.header("Build your own fruit smoothie")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
